4e6ec52a36b8fc28f1a1ace6262ca8900dfa6554
a.salem <abdulkarimsalem1997@gmail.com>
2024-11-13 22:34:25 +0300

    create dashboard desktop layout

---
+++ lib/widgets/dashboard_disktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardDisktopLayout extends StatelessWidget {
+  const DashboardDisktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
