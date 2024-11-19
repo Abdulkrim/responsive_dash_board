b61abec95c7449ec404b29d96ac4fdd0527d2010
a.salem <abdulkarimsalem1997@gmail.com>
2024-11-19 16:24:14 +0300

    display drawer items

--- lib/models/drawer_item_model.dart
+++ lib/models/drawer_item_model.dart
@@ -2,6 +2,6 @@ class DrawerItemModel {
  final String image;
 final String title;
 
-DrawerItemModel({required this.image, required this.title});
+const DrawerItemModel({required this.image, required this.title});
 
 }
