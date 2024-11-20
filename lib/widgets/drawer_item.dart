9f11a6f476701e913e566331fecff4cf5272bb98
a.salem <abdulkarimsalem1997@gmail.com>
2024-11-20 11:21:47 +0300

    create active drawer item

--- lib/widgets/drawer_item.dart
+++ lib/widgets/drawer_item.dart
@@ -10,11 +10,49 @@ class DrawerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return isActive ?  ActiveDrawerItem(drawerItemModel: drawerItemModel,) : InActiveDrawerItem(drawerItemModel: drawerItemModel);
+  }
+}
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
       title: Text(
         drawerItemModel.title,
-        style: AppStyles.styleRegular16(context),
+        style: AppStyles.styleMedium16(context),
+      ),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleBold16(context),
+      ),
+      trailing: Container(
+        width: 3.27,
+        color: const Color(0xFF4EB7F2),
       ),
     );
   }
