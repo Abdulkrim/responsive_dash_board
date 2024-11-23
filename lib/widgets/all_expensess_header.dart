f09da1273ad3d6c39550e4baf8eb2ed1fc388908
a.salem <abdulkarimsalem1997@gmail.com>
2024-11-23 23:48:36 +0300

    all expensess part 2

--- lib/widgets/all_expensess_header.dart
+++ lib/widgets/all_expensess_header.dart
@@ -1,4 +1,7 @@
+import 'range_options.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
 
 class AllExpensessHeader extends StatelessWidget {
   const AllExpensessHeader({super.key});
@@ -7,7 +10,12 @@ class AllExpensessHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        
+        Text(
+          "All Expenses",
+          style: AppStyles.styleSemiBold20(context)
+        ),
+        const Expanded(child: SizedBox()),
+        const RangeOptions(),
       ],
     );
   }
