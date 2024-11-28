f36ae24620aec53cdade50c7f2c63c5e3f3fc4d8
a.salem <abdulkarimsalem1997@gmail.com>
2024-11-28 09:52:28 +0300

    latest transactoin part one

---
+++ lib/widgets/latest_transacton_list_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LatestTransactionListView extends StatelessWidget {
+  const LatestTransactionListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
