e411ba3cb6a490cdd98ec6bc3ed55f593c5d3911
a.salem <abdulkarimsalem1997@gmail.com>
2024-12-07 22:11:34 +0300

    my cards section

--- lib/widgets/dashboard_disktop_layout.dart
+++ lib/widgets/dashboard_disktop_layout.dart
@@ -1,9 +1,10 @@
 import 'my_card_page_view.dart';
+import 'my_cards_sections.dart';
 import 'package:flutter/material.dart';
 import 'all_expensess_and_quick_invoice_item.dart';
+import 'package:responsive_dash_board/widgets/custom_dot.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-
-
+import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 
 class DashboardDisktopLayout extends StatelessWidget {
   const DashboardDisktopLayout({super.key});
@@ -15,15 +16,12 @@ class DashboardDisktopLayout extends StatelessWidget {
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
         Expanded(
-          flex: 2,
-          child: SingleChildScrollView(
-            child: AllExpensessAndQuickInvoiceSection(),
-          )),
-           SizedBox(width: 24),
-        Expanded(
-          child: MyCardPageView()
-          ),  
-          
+            flex: 2,
+            child: SingleChildScrollView(
+              child: AllExpensessAndQuickInvoiceSection(),
+            )),
+        SizedBox(width: 24),
+        Expanded(child: MyCardsSections())
       ],
     );
   }
---
+++ lib/widgets/my_cards_sections.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/dots_indicator.dart';
+import 'package:responsive_dash_board/widgets/my_card_page_view.dart';
+
+class MyCardsSections extends StatelessWidget {
+  const MyCardsSections({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("My card",style: AppStyles.styleSemiBold20(context),),
+        const SizedBox(height: 20,),
+        const MyCardPageView(),
+        const SizedBox(height: 20,),
+        const DotsIndicator()
+      ],
+    );
+  }
+}
