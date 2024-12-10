8494b1dd2fe65c66c3962d2b00926bc39c4d8963
a.salem <abdulkarimsalem1997@gmail.com>
2024-12-10 22:16:22 +0300

    income details

--- lib/widgets/income_chart.dart
+++ lib/widgets/income_chart.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-
 class IncomeChart extends StatefulWidget {
   const IncomeChart({super.key});
 
@@ -15,25 +14,48 @@ class _IncomeChartState extends State<IncomeChart> {
 
   @override
   Widget build(BuildContext context) {
-    return PieChart( getChartData());
+    return AspectRatio(
+      aspectRatio: 1,
+      child: PieChart(getChartData()),
+    );
   }
 
   PieChartData getChartData() {
     return PieChartData(
-      pieTouchData: PieTouchData(
-        enabled: true,
-        touchCallback: (p0, pieTouchResponse) {
-        activeIndex = pieTouchResponse?.touchedSection?.touchedSectionIndex?? -1;
-        setState(() {});
-        },
-      ),
-      sectionsSpace: 0,
-      sections: [
-        PieChartSectionData(showTitle: false ,value: 40 ,radius: activeIndex == 0 ? 60 : 50,color: const Color(0xFF208CC8),),
-        PieChartSectionData(showTitle: false ,value: 25 ,radius: activeIndex == 1 ? 60 :50,color: const Color(0xFF4EB7F2),),
-        PieChartSectionData(showTitle: false ,value: 20 ,radius: activeIndex == 2 ? 60 :50,color: const Color(0xFF064061),),
-        PieChartSectionData(showTitle: false ,value: 22 ,radius: activeIndex == 3 ? 60 :50,color: const Color(0xFFE2DECD),),
-      ]
-    );
+        pieTouchData: PieTouchData(
+          enabled: true,
+          touchCallback: (p0, pieTouchResponse) {
+            activeIndex =
+                pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
+          },
+        ),
+        sectionsSpace: 0,
+        sections: [
+          PieChartSectionData(
+            showTitle: false,
+            value: 40,
+            radius: activeIndex == 0 ? 60 : 50,
+            color: const Color(0xFF208CC8),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            value: 25,
+            radius: activeIndex == 1 ? 60 : 50,
+            color: const Color(0xFF4EB7F2),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            value: 20,
+            radius: activeIndex == 2 ? 60 : 50,
+            color: const Color(0xFF064061),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            value: 22,
+            radius: activeIndex == 3 ? 60 : 50,
+            color: const Color(0xFFE2DECD),
+          ),
+        ]);
   }
 }
---
+++ lib/widgets/item_details.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../models/item_details_model.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+
+class ItemDetails extends StatelessWidget {
+  const ItemDetails({super.key, required this.itemDetailsModel});
+  final ItemDetailsModel itemDetailsModel ;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        height: 12,
+        width: 12,
+        decoration:  ShapeDecoration(
+          color: itemDetailsModel.color,
+          shape: const OvalBorder()
+        ),
+      ),
+      title: Text(
+        itemDetailsModel.title,
+        style: AppStyles.styleRegular16(context),
+      ),
+      trailing: Text(
+       itemDetailsModel.value,
+        style: AppStyles.styleMedium16(context),
+      ),
+    );
+  }
+}
